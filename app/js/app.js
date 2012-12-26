c02a508678ec88eede6aac05df6fcfecffd7e739
Jiang Tao Hong <jiang.hong@mail.utoronto.ca>
2012-12-26 11:10:52 -0800

    preload splashes

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,7 @@
 angular.module('clolset', ['clolset.filters', 'clolset.services', 'clolset.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/champs', {templateUrl: 'partials/champ-grid.html', controller: ChampGridCtrl});
+    $routeProvider.when('/', {redirectTo: '/champs'});
     $routeProvider.when('/:champName', {templateUrl: 'partials/champ-detail.html', controller: ChampDetailCtrl});
     $routeProvider.otherwise({redirectTo: '/champs'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,15 +5,28 @@
 
 function ChampGridCtrl($scope, Square) {
 	$scope.squares = Square.query();
+	/*
+	$scope.submit = function () {
+		if (this.squares.length == 1) {
+
+		}
+	}*/
 }
 ChampGridCtrl.$inject = ['$scope', 'Square'];
 
 
 function ChampDetailCtrl($scope, Champ, $routeParams) {
 	$scope.champName = $routeParams.champName;
+	$scope.hideValue = true;
+	$scope.showInfo = function() {
+		$scope.hideValue = false;
+	}
+	$scope.hideInfo = function() {
+		$scope.hideValue = true;
+	}
 	$scope.champs = Champ.get({}, function(champs){
 		$scope.mainImg = champs[$routeParams.champName].splashes[0];
-		
+		$scope.imageList = champs[$scope.champName].splashes;
 		$scope.setImage = function(imageUrl){
 			var indexLen = imageUrl.length - 6; 
 			// index is the last 6 letters of imageUrl, i.e _0.jpg, _1.jpg
