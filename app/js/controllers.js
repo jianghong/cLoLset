990873ff82cd1825a8d6f3bc1a5574bab0ae38f4
Jackson <Jackson@Jacksons-Mac-mini.local>
2013-01-16 22:37:41 -0500

    bleh

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,8 +12,7 @@ function ChampGridCtrl($scope, Square, autoCompleteDataService, $location) {
 }
 //ChampGridCtrl.$inject = ['$scope', 'Square', "autoCompleteDataService", '$location'];
 
-
-function ChampDetailCtrl($scope, Champ, Skin, $routeParams, autoCompleteDataService, $location) {
+function ChampDetailCtrl($scope, Champ, Skin, $routeParams, autoCompleteDataService, $location, $filter) {
 	$scope.champName = $routeParams.champName;
 	$scope.availableTags = autoCompleteDataService.getSource();
 	$scope.neighbors = autoCompleteDataService.getNeighbors($scope.champName);
@@ -26,7 +25,6 @@ function ChampDetailCtrl($scope, Champ, Skin, $routeParams, autoCompleteDataServ
 			$location.path("/"+$scope.typeaheadValue);
 		}
 	}
-
 	$scope.hideValue = true;
 	$scope.showInfo = function() {
 		$scope.hideValue = false;
@@ -34,20 +32,27 @@ function ChampDetailCtrl($scope, Champ, Skin, $routeParams, autoCompleteDataServ
 	$scope.hideInfo = function() {
 		$scope.hideValue = true;
 	}
-	
 	$scope.champs = Champ.get({}, function(champs){
 		$scope.mainImg = champs[$scope.champName].splashes[0];
-		$scope.mainSkin = $scope.mainImg.substring(17, ($scope.mainImg.length-11));
+		$scope.ms = $scope.mainImg.substring(17, ($scope.mainImg.length-11));
+		$scope.mainSkin = $filter('spaceName')($scope.ms);
+	/*$scope.skinDetails = Skin.get({}, function(skins){
+		$scope.mainRP = $scope.skinDetails[$scope.ms].rp;
+		$scope.mainIP = $scope.skinDetails[$scope.ms].ip;
+		$scope.hideIPValue = false;
+	});*/
+	$scope.showResponse = function(response) {
+		var responseString = response["items"][0]["id"]["videoId"];
+		var responseString2 = response["items"][1]["id"]["videoId"];
+		$scope.response = "http://www.youtube.com/embed/"+ responseString; 
+		$scope.response2 = "http://www.youtube.com/embed/"+ responseString2;
+	};
+	$scope.request = search($scope.mainSkin);
+	$scope.request.execute($scope.showResponse);
 
-		$scope.skinDetails = Skin.get({}, function(skins){
-			$scope.mainRP = $scope.skinDetails[$scope.mainSkin].rp;
-			$scope.mainIP = $scope.skinDetails[$scope.mainSkin].ip;
-			$scope.hideIPValue = false;
-		})
-
-		$scope.imageList = champs[$scope.champName].splashes;
-		$scope.setImage = function(imageUrl){
-			var indexLen = imageUrl.length - 10; 
+	$scope.imageList = champs[$scope.champName].splashes;
+	$scope.setImage = function(imageUrl){
+		var indexLen = imageUrl.length - 10; 
 			// index is the last 6 letters of imageUrl, i.e _0.jpg, _1.jpg
 			var index = imageUrl.substring(17, indexLen);
 			// find the corresponding splash image
@@ -55,15 +60,18 @@ function ChampDetailCtrl($scope, Champ, Skin, $routeParams, autoCompleteDataServ
 			for (var i=0; i<splashArray.length; i++){
 				if (splashArray[i].indexOf(index) !== -1) {
 					$scope.mainImg = splashArray[i];
-					$scope.mainSkin = $scope.mainImg.substring(17, ($scope.mainImg.length-11));
-					$scope.mainRP = $scope.skinDetails[$scope.mainSkin].rp;
-					if ($scope.skinDetails[$scope.mainSkin].ip === 0){
+					$scope.ms= $scope.mainImg.substring(17, ($scope.mainImg.length-11));
+					$scope.mainSkin = $filter('spaceName')($scope.ms);
+					$scope.request = search($scope.mainSkin);
+					$scope.request.execute($scope.showResponse);
+					/*$scope.mainRP = $scope.skinDetails[$scope.ms].rp;
+					if ($scope.skinDetails[$scope.ms].ip === 0){
 						$scope.hideIPValue = true;
 					}
 					else {
-						$scope.mainIP = $scope.skinDetails[$scope.mainSkin].ip;
+						$scope.mainIP = $scope.skinDetails[$scope.ms].ip;
 						$scope.hideIPValue = false;
-					}
+					}*/
 					break;
 				}
 			}
